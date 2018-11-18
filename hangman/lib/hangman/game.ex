940b8cc88f345d962644c61c3fa84995136897f7
Bijan <bijanbwb@gmail.com>
2018-11-18 11:25:50 -0500

    Features to make moves

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -2,7 +2,8 @@ defmodule Hangman.Game do
   defstruct(
     game_state: :initializing,
     letters: [],
-    turns_left: 7
+    turns_left: 7,
+    used: MapSet.new()
   )
 
   def new_game() do
@@ -11,5 +12,25 @@ defmodule Hangman.Game do
         Dictionary.random_word()
         |> String.codepoints()
     }
+  end
+
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+    {game, tally(game)}
+  end
+  def make_move(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    {game, tally(game)}
+  end
+
+  def accept_move(game, _guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+  end
+
+  def tally(_game) do
+    123
   end
 end
