7b50e8f4d4825fa48ade8c64faeb839298410873
Bijan <bijanbwb@gmail.com>
2018-11-18 14:01:13 -0500

    Feature for a correct guess

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -6,6 +6,11 @@ defmodule Hangman.Game do
     used: MapSet.new()
   )
 
+  def new_game(word) do
+    %Hangman.Game{
+      letters: word |> String.codepoints()
+    }
+  end
   def new_game() do
     %Hangman.Game{
       letters:
@@ -25,12 +30,27 @@ defmodule Hangman.Game do
   def accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
-
   def accept_move(game, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score_guess(Enum.member?(game.letters, guess))
+  end
+
+  def score_guess(game, _good_guess = true) do
+    new_state =
+      MapSet.new(game.letters)
+      |> MapSet.subset?(game.used)
+      |> maybe_won()
+
+    Map.put(game, :game_state, new_state)
+  end
+  def score_guess(game, _good_guess) do
+    game
   end
 
   def tally(_game) do
     123
   end
+
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
 end
