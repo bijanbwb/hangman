77de1336140e7ea5274892aa5ef9ed56db2997df
Bijan <bijanbwb@gmail.com>
2018-11-18 14:35:00 -0500

    Refactor into public API and private functions

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -6,36 +6,43 @@ defmodule Hangman.Game do
     used: MapSet.new()
   )
 
+  # Public
+
   def new_game(word) do
     %Hangman.Game{
       letters: word |> String.codepoints()
     }
   end
   def new_game() do
-    %Hangman.Game{
-      letters:
-        Dictionary.random_word()
-        |> String.codepoints()
-    }
+    new_game(Dictionary.random_word())
   end
 
   def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
-    {game, tally(game)}
+    game
   end
   def make_move(game, guess) do
-    game = accept_move(game, guess, MapSet.member?(game.used, guess))
-    {game, tally(game)}
+    accept_move(game, guess, MapSet.member?(game.used, guess))
   end
 
-  def accept_move(game, _guess, _already_guessed = true) do
+  def tally(game) do
+    %{
+      game_state: game.game_state,
+      letters: game.letters |> reveal_guessed(game.used),
+      turns_left: game.turns_left
+    }
+  end
+
+  # Private
+
+  defp accept_move(game, _guess, _already_guessed = true) do
     Map.put(game, :game_state, :already_used)
   end
-  def accept_move(game, guess, _already_guessed) do
+  defp accept_move(game, guess, _already_guessed) do
     Map.put(game, :used, MapSet.put(game.used, guess))
     |> score_guess(Enum.member?(game.letters, guess))
   end
 
-  def score_guess(game, _good_guess = true) do
+  defp score_guess(game, _good_guess = true) do
     new_state =
       MapSet.new(game.letters)
       |> MapSet.subset?(game.used)
@@ -43,20 +50,24 @@ defmodule Hangman.Game do
 
     Map.put(game, :game_state, new_state)
   end
-  def score_guess(game = %{turns_left: 1}, _not_good_guess) do
+  defp score_guess(game = %{turns_left: 1}, _not_good_guess) do
     Map.put(game, :game_state, :lost)
   end
-  def score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
+  defp score_guess(game = %{turns_left: turns_left}, _not_good_guess) do
     %{game |
       game_state: :bad_guess,
       turns_left: turns_left - 1
     }
   end
 
-  def tally(_game) do
-    123
+  defp maybe_won(true), do: :won
+  defp maybe_won(_), do: :good_guess
+
+  defp reveal_guessed(letters, used) do
+    letters
+    |> Enum.map(fn(l) -> reveal_letter(l, MapSet.member?(used, l)) end)
   end
 
-  def maybe_won(true), do: :won
-  def maybe_won(_), do: :good_guess
+  defp reveal_letter(letter, _in_word = true), do: letter
+  defp reveal_letter(_letter, _not_in_word), do: "_"
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -14,13 +14,15 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost games" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert {^game, _tally} = Game.make_move(game, "x")
+
+      assert ^game = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
-    game = Game.new_game()
-    {game, _tally} = Game.make_move(game, "x")
+    game =
+      Game.new_game()
+      |> Game.make_move("x")
 
     assert game.game_state != :already_used
   end
@@ -28,49 +30,53 @@ defmodule GameTest do
   test "second occurrence of letter is not already used" do
     game = Game.new_game()
  
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
 
-    {game, _tally} = Game.make_move(game, "x")
+    game = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
-    game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "w")
+    game =
+      Game.new_game("wibble")
+      |> Game.make_move("w")
 
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a bad guess is recognized" do
-    game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "x")
+    game =
+      Game.new_game("wibble")
+      |> Game.make_move("x")
 
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
   test "a guessed word is a won game" do
-    game = Game.new_game("wibble")
-    {game, _tally} = Game.make_move(game, "w")
-    {game, _tally} = Game.make_move(game, "i")
-    {game, _tally} = Game.make_move(game, "b")
-    {game, _tally} = Game.make_move(game, "l")
-    {game, _tally} = Game.make_move(game, "e")
+    game =
+      Game.new_game("wibble")
+      |> Game.make_move("w")
+      |> Game.make_move("i")
+      |> Game.make_move("b")
+      |> Game.make_move("l")
+      |> Game.make_move("e")
 
     assert game.game_state == :won
   end
 
   test "seven bad guesses is a lost game" do
-    game = Game.new_game("w")
-    {game, _tally} = Game.make_move(game, "a")
-    {game, _tally} = Game.make_move(game, "b")
-    {game, _tally} = Game.make_move(game, "c")
-    {game, _tally} = Game.make_move(game, "d")
-    {game, _tally} = Game.make_move(game, "e")
-    {game, _tally} = Game.make_move(game, "f")
-    {game, _tally} = Game.make_move(game, "g")
+    game =
+      Game.new_game("w")
+      |> Game.make_move("a")
+      |> Game.make_move("b")
+      |> Game.make_move("c")
+      |> Game.make_move("d")
+      |> Game.make_move("e")
+      |> Game.make_move("f")
+      |> Game.make_move("g")
 
     assert game.game_state == :lost
   end
