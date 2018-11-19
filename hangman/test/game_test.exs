4c06b4210ccd0e070167a46c9a2874f6cb04fc30
Bijan <bijanbwb@gmail.com>
2018-11-19 17:49:41 -0500

    Don't fail word_list for fun

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -15,68 +15,63 @@ defmodule GameTest do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
 
-      assert ^game = Game.make_move(game, "x")
+      assert {^game, _tally} = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
-    game =
-      Game.new_game()
-      |> Game.make_move("x")
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
 
     assert game.game_state != :already_used
   end
 
   test "second occurrence of letter is not already used" do
     game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
  
-    game = Game.make_move(game, "x")
     assert game.game_state != :already_used
 
-    game = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
-    game =
-      Game.new_game("wibble")
-      |> Game.make_move("w")
+    game = Game.new_game("wibble")
+    {game, _tally} = Game.make_move(game, "w")
 
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
   test "a bad guess is recognized" do
-    game =
-      Game.new_game("wibble")
-      |> Game.make_move("x")
+    game = Game.new_game("wibble")
+    {game, _tally} = Game.make_move(game, "x")
 
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
   test "a guessed word is a won game" do
-    game =
-      Game.new_game("wibble")
-      |> Game.make_move("w")
-      |> Game.make_move("i")
-      |> Game.make_move("b")
-      |> Game.make_move("l")
-      |> Game.make_move("e")
+    game = Game.new_game("wibble")
+    {game, _tally} = Game.make_move(game, "w")
+    {game, _tally} = Game.make_move(game, "i")
+    {game, _tally} = Game.make_move(game, "b")
+    {game, _tally} = Game.make_move(game, "l")
+    {game, _tally} = Game.make_move(game, "e")
 
     assert game.game_state == :won
   end
 
   test "seven bad guesses is a lost game" do
-    game =
-      Game.new_game("w")
-      |> Game.make_move("a")
-      |> Game.make_move("b")
-      |> Game.make_move("c")
-      |> Game.make_move("d")
-      |> Game.make_move("e")
-      |> Game.make_move("f")
-      |> Game.make_move("g")
+    game = Game.new_game("w")
+    {game, _tally} = Game.make_move(game, "a")
+    {game, _tally} = Game.make_move(game, "b")
+    {game, _tally} = Game.make_move(game, "c")
+    {game, _tally} = Game.make_move(game, "d")
+    {game, _tally} = Game.make_move(game, "e")
+    {game, _tally} = Game.make_move(game, "f")
+    {game, _tally} = Game.make_move(game, "g")
 
     assert game.game_state == :lost
   end
