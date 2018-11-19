63a902880e7b16540f5b56e6dcc25c8678a33736
Bijan <bijanbwb@gmail.com>
2018-11-19 10:44:17 -0500

    Add process example

---
+++ procs.exs
@@ -0,0 +1,18 @@
+defmodule Procs do
+  def greeter(count) do
+    receive do
+      {:boom, reason} ->
+        exit(reason)
+
+      {:add, n} ->
+        greeter(count + n)
+
+      :reset ->
+        greeter(0)
+
+      msg ->
+        IO.puts("#{count}: #{msg}")
+        greeter(count)
+    end
+  end
+end
