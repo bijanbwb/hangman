2c19d5fbf08bbbf9dbe92fbea3e33df9d0606739
Bijan <bijanbwb@gmail.com>
2018-11-20 13:17:34 -0500

    Working view and templates

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,34 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  @responses %{
+    :won => {:outline, "You won!"},
+    :lost => {:outline, "You lost"},
+    :good_guess => {:outline, "Good guess!"},
+    :bad_guess => {:outline, "Bad guess"},
+    :already_used => {:outline, "You already guessed that"}
+  }
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def game_over?(%{game_state: game_state}) do
+    game_state in [:won, :lost]
+  end
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+  defp alert({class, message}) do
+    """
+    <div class="button button-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
 end
