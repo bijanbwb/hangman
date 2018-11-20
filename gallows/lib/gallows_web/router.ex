074143a822ddb1f875f9e9919976c675a04bdcc2
Bijan <bijanbwb@gmail.com>
2018-11-20 12:49:35 -0500

    Working form interaction

--- gallows/lib/gallows_web/router.ex
+++ gallows/lib/gallows_web/router.ex
@@ -17,6 +17,10 @@ defmodule GallowsWeb.Router do
     pipe_through :browser
 
     get "/", HangmanController, :new_game
+
+    post "/", HangmanController, :create_game
+
+    put "/", HangmanController, :make_move
   end
 
   # Other scopes may use custom stacks.
