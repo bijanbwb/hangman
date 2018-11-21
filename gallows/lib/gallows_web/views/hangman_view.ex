d0298095eb7576b7b5bef9524c01d0f8636736d3
Bijan <bijanbwb@gmail.com>
2018-11-21 15:56:57 -0500

    Add hangman SVG and milligram grid

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -10,4 +10,12 @@ defmodule GallowsWeb.HangmanView do
   def game_over?(%{game_state: game_state}) do
     game_state in [:won, :lost]
   end
+
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
 end
